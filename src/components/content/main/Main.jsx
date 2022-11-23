c9eb386560358bfb1240f85fb26ec016cd276c68
SergeyNasibulin <serega-nasibulin@yandex.ru>
2022-11-23 15:42:36 +0300

    Added posts grid

--- src/components/content/main/Main.jsx
+++ src/components/content/main/Main.jsx
@@ -2,8 +2,56 @@ import styles from "./Main.module.css"
 
 let Main = (props) => {
     return (
-        <div>
-            Main
+        <div className={styles.mainCont}>
+            <div className={styles.lastPosts}>
+
+                <div className={styles.lastPost}>
+                    <div className={styles.description}>
+                        <div className={styles.name}>Название поста</div>
+                        <div className={styles.text}>Описание поста, а точнее текст из начала статьи...</div>
+                        <div className={styles.links}>#хештеги</div>
+                    </div>
+                </div>
+
+                <div className={styles.lastPost}>
+                    <div className={styles.description}>
+                        <div className={styles.name}>Название поста</div>
+                        <div className={styles.text}>Описание поста, а точнее текст из начала статьи...</div>
+                        <div className={styles.links}>#хештеги</div>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className={styles.posts}>
+
+                <div className={styles.post}>
+                    <div className={styles.description}>
+                        <div className={styles.name}>Название поста</div>
+                        <div className={styles.text}>Описание поста, а точнее текст из начала статьи...</div>
+                        <div className={styles.links}>#хештеги</div>
+                    </div>
+                </div>
+
+                <div className={styles.post}>
+                    <div className={styles.description}>
+                        <div className={styles.name}>Название поста</div>
+                        <div className={styles.text}>Описание поста, а точнее текст из начала статьи...</div>
+                        <div className={styles.links}>#хештеги</div>
+                    </div>
+                </div>
+
+                <div className={styles.post}>
+                    <div className={styles.description}>
+                        <div className={styles.name}>Название поста</div>
+                        <div className={styles.text}>Описание поста, а точнее текст из начала статьи...</div>
+                        <div className={styles.links}>#хештеги</div>
+                    </div>
+                </div>
+
+            </div>
+           
         </div> 
     );
 }
