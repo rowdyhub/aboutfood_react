0294e1e63de1f807ea84e5c236981090cb9f24d8
SergeyNasibulin <serega-nasibulin@yandex.ru>
2023-02-28 15:06:47 +0300

    Created new component

--- src/components/content/recipes/Recipe.jsx
+++ src/components/content/recipes/Recipe.jsx
@@ -9,29 +9,67 @@ let Recipe = () => {
     /* Тут будет запрос рецепта по id */
 
     return (
-        <div class={ styles.content }>
-            <div class={ styles.recipe_header }>
-                <div class={ styles.recipe_header_photo }></div>
-                <div class={ styles.recipe_header_data }>
-                    <div class={ styles.recipe_white_block }>
-                        <div class={ styles.recipe_author_block }>
+        <div className={ styles.content }>
+            <div className={ styles.recipe_header }>
+                <div className={ styles.recipe_header_photo }></div>
+                <div className={ styles.recipe_header_data }>
+                    <div className={ styles.recipe_white_block }>
+                        <div className={ styles.recipe_author_block }>
                             <div className={ styles.recipe_author_avatar }></div>
                             <div className={ styles.recipe_author_name }>doshik</div>
                         </div>
-                        <div class={ styles.recipe_control }>
+                        <div className={ styles.recipe_control }>
                             <div className={ styles.recipe_likes }>13</div>
                             <div className={ styles.recipe_button }>
                                 <FavoriteButton action='false'/>
                             </div>
                         </div>
                     </div>
-                    <div class={ styles.recipe_header_types }>Русская кухня - Супы</div>
-                    <div class={ styles.recipe_header_name }>name</div>
-                    <div class={ styles.recipe_header_stats }>positions</div>
+                    <div className={ styles.recipe_header_types }>Русская кухня - Супы</div>
+                    <div className={ styles.recipe_header_name }>Сырный суп по-французски с курицей (место для второй строки при длинном названии)</div>
+                    <div className={ styles.recipe_header_stats }>
+                        <div className={ styles.persones }>4 порции</div>
+                        <div className={ styles.time }>60 минут</div>
+                        <div className={ styles.likes }>217 отметок</div>
+                    </div>
                 </div>
             </div>
-            <div>description</div>
-            <div>steps</div>
+
+            <div className={ styles.recipe_description }>
+                Нежный суп с приятным сливочным вкусом перенесет Вас прямиком на улицы Парижа. Превосходное сочетание сырного супа и хрустящихсухариков никого не оставит равнодушным.
+            </div>
+
+            <div className={ styles.recipe_ingridients }>
+                <div className={ styles.recipe_ingridient }>Вода - 1 л.</div>
+                <div className={ styles.recipe_ingridient }>Кортофель - 0,5 кг.</div>
+                <div className={ styles.recipe_ingridient }>Лук - 100 гр.</div>
+            </div>
+
+            <div className={ styles.recipe_steps }>
+
+                <div className={ styles.recipe_step }>
+                    <div className={ styles.recipe_step_photo }></div>
+                    <div className={ styles.recipe_step_text }> Описание шаг 1 </div>
+                </div>
+
+                <div className={ styles.recipe_step }>
+                    <div className={ styles.recipe_step_photo }></div>
+                    <div className={ styles.recipe_step_text }> Описание шаг 2 </div>
+                </div>
+
+                <div className={ styles.recipe_step }>
+                    <div className={ styles.recipe_step_photo }></div>
+                    <div className={ styles.recipe_step_text }> Описание шаг 3 </div>
+                </div>
+
+                <div className={ styles.recipe_step }>
+                    <div className={ styles.recipe_step_photo }></div>
+                    <div className={ styles.recipe_step_text }> Описание шаг 4 </div>
+                </div>
+
+            </div>
+
+
         </div>
     )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,7 +10,7 @@ let Footer = () => {
                     <div className={ styles.row }>© ABOUT FOOD, 2021-2022</div>
                     <div className={ styles.row }>Перепечатка материалов данного сайта возможна только с письменного разрешения редакции. При цитировании ссылка на <a href="www.aboutfood.ru">www.aboutfood.ru</a> обязательна.</div>
                     <div className={ styles.row }>Email: <a>info@aboutfood.ru</a></div>
-                    <div className={ styles.row }>Дизайн: <a>Sergey Nasibulin</a></div>
+                    <div className={ styles.row }>Дизайн: <a>Dari</a></div>
                 </div>
                 <div className={ styles.column }>
                     <Link className={ styles.link } to='/'>Главная</Link>
